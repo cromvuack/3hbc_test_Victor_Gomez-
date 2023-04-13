ff8eec54c6f7df5029cd1eaa3efa33e1fbd38954
Victor Gomez <cromvuack@gmail.com>
2023-04-13 10:02:10 -0600

    feat: airports crud creation

---
+++ app/Http/Controllers/AirportController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Airport;
+
+class AirportController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $airports = Airport::all();
+        return $airports;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $airport = new Airport();
+        $airport->name = $request->name;
+        $airport->code = $request->code;
+        $airport->city = $request->city;
+        $airport->save();
+        return $airport;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        $airport = Airport::findOrFail($request->id);
+        $airport->name = $request->name;
+        $airport->code = $request->code;
+        $airport->city = $request->city;
+        $airport->save();
+        return $airport;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request)
+    {
+        $airport = Airport::destroy($request->id);
+        return $airport;
+    }
+}
